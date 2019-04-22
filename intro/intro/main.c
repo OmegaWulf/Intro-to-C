77395dcc203278ac016909da2bc3a68dae4b47af
Nikita Thomas <nikcxz@gmail.com>
2019-04-22 19:32:05 -0400

    Added xcode

---
+++ intro/intro/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  intro
+//
+//  Created by Nikita Thomas on 4/22/19.
+//  Copyright © 2019 Nikita Thomas. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
